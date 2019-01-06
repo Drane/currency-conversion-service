6301f4719f27c9c903c5ebbf31282f6093389a47
Jochen.Szostek <jochen.szostek@economie.fgov.be>
2019-01-06 15:22:15 +0100

    implemented Eureka

--- src/main/java/com/prefabsoft/micro/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/prefabsoft/micro/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,11 +2,13 @@ package com.prefabsoft.micro.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableFeignClients("com.prefabsoft.micro.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
