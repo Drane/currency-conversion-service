9d4cb78cf90024b571345311551cd18ce374bd72
Jochen.Szostek <jochen.szostek@economie.fgov.be>
2019-01-06 18:07:28 +0100

    used zuul api gateway to call currency exchange service

--- src/main/java/com/prefabsoft/micro/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/prefabsoft/micro/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -6,11 +6,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     //TODO: https://www.udemy.com/microservices-with-spring-boot-and-spring-cloud/learn/v4/t/lecture/8005762?start=15 => PathVariable params not necessary?
     CurrencyConversionBean retrieveExchangeValue(@PathVariable(name = "from") String from, @PathVariable(name = "to") String to);
 
